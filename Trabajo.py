eef597ca60695a991d7df89431323ca195aa1801
alexmendoza <alexmendoza.pacheco@hotmail.com>
2021-09-04 18:41:04 -0500

    agregando 2 ejercicio

--- Trabajo.py
+++ Trabajo.py
@@ -3,3 +3,13 @@ def punto1():
     return print ("el resultado es de: ", int(y))
 
 punto1()
+
+
+def punto2():
+    z = 5
+    n = 3
+    m = z - n
+    y = (((z + 2 - n)**2 * m + 8 / 2 - 30) / 2 * 5 - 3 )**5 + 15 * 3 - 9 / 3
+    return print("el resultado es de: ", y)
+
+punto2()
